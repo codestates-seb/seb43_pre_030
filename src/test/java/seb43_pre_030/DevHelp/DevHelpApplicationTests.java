a7bbe66f0b88b21ecf81ef05442fe3747af61623
mins <113077033+mins-git@users.noreply.github.com>
2023-04-12 11:24:53 +0900

    Add files via upload

---
+++ src/test/java/seb43_pre_030/DevHelp/DevHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package seb43_pre_030.DevHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
