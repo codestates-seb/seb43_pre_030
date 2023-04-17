081e6e90c4e26b6a8efc2c9ed208f85f9c14f520
tari45800 <tari45800@gmail.com>
2023-04-18 00:15:19 +0900

    feat:WritingGuideBox 컴포넌트 추가

---
+++ client/src/components/questionPageComponents/WritingGuideBox.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const WritingGuideBoxContainer = styled.div`
+
+`
+
+
+function WritingGuideBox() {
+
+
+  return (
+    <WritingGuideBoxContainer>
+      <div>태스트입니다</div>
+    </WritingGuideBoxContainer>
+  );
+}
+
+export default WritingGuideBox;
+
