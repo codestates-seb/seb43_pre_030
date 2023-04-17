5eee8d1ab1640b269ffe85a0d165dce800e6f4ae
tari45800 <tari45800@gmail.com>
2023-04-18 00:14:12 +0900

    feat:TipBox 컴포넌트 추가

---
+++ client/src/components/questionPageComponents/TipBox.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const TipBoxContainer = styled.div`
+
+`
+
+
+function TipBox() {
+
+
+  return (
+    <TipBoxContainer>
+      <div>태스트입니다</div>
+    </TipBoxContainer>
+  );
+}
+
+export default TipBox;
