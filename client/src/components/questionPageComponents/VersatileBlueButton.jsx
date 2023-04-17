5314073ea5653e02c96a4c849a25386146d707a1
tari45800 <tari45800@gmail.com>
2023-04-18 00:14:34 +0900

    feat:VersatileBlueButton 컴포넌트 추가

---
+++ client/src/components/questionPageComponents/VersatileBlueButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const VersatileBlueButtonContainer = styled.div`
+
+`
+
+
+function VersatileBlueButton() {
+
+
+  return (
+    <VersatileBlueButtonContainer>
+      <div>태스트입니다</div>
+    </VersatileBlueButtonContainer>
+  );
+}
+
+export default VersatileBlueButton;
