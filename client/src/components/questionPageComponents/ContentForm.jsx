1f7178516244d96f92ad369dbaf149288c15d17d
tari45800 <tari45800@gmail.com>
2023-04-18 00:13:52 +0900

    feat:ContentForm 컴포넌트 추가

---
+++ client/src/components/questionPageComponents/ContentForm.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const ContentFormContainer = styled.div`
+
+`
+
+
+function ContentForm() {
+
+
+  return (
+    <ContentFormContainer>
+      <div>태스트입니다</div>
+    </ContentFormContainer>
+  );
+}
+
+export default ContentForm;
