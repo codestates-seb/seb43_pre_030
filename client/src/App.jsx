cccf61d81ac729ed2f3bf9ebd192ba14ee3fbac1
crowwan <crowwan@naver.com>
2023-04-17 13:39:35 +0900

    feat: App 컴포넌트 배경색 추가, 해더 컴포넌트 사용

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import styled from "styled-components";
+import Header from "./layouts/Header";
 import { GlobalStyles } from "./styles/GlobalStyles";
+
+const StyledBodyContainer = styled.div`
+  background-color: #f8f9f9;
+`;
 
 function App() {
   return (
-    <>
+    <StyledBodyContainer>
       <GlobalStyles />
+      <Header />
       <div className="App">test</div>
-    </>
+    </StyledBodyContainer>
   );
 }
 
