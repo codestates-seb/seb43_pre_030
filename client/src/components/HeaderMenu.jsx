1f9f34a7e949ec31e3fca078318be0e18b67901d
crowwan <crowwan@naver.com>
2023-04-17 17:01:45 +0900

    feat: 드롭다운 적용 중

--- client/src/components/HeaderMenu.jsx
+++ client/src/components/HeaderMenu.jsx
@@ -1,6 +1,7 @@
 import { AiOutlineUser } from "react-icons/ai";
 import { BsMenuUp } from "react-icons/bs";
 import styled from "styled-components";
+import { StyledDropDown } from "../styles/StyledDropDown";
 
 const StyledMenuContainer = styled.div`
   display: flex;
@@ -10,10 +11,27 @@ const StyledMenuContainer = styled.div`
     cursor: pointer;
   }
 `;
+const StyledUserDropDown = styled(StyledDropDown)`
+  min-width: 250px;
+  transform: translateX(-100px);
+  &::after {
+    transform: translateX(95px) rotate(45deg);
+  }
+`;
+
+function UserDropDown() {
+  return (
+    <div style={{ position: "relative" }}>
+      <AiOutlineUser size="2rem" color="var(--font-color-light)" />
+      <StyledUserDropDown>text</StyledUserDropDown>
+    </div>
+  );
+}
 
 function HeaderMenu() {
   return (
     <StyledMenuContainer>
+      {/* <UserDropDown /> */}
       <AiOutlineUser size="2rem" color="var(--font-color-light)" />
       <BsMenuUp size="2rem" color="var(--font-color-light)" />
     </StyledMenuContainer>
