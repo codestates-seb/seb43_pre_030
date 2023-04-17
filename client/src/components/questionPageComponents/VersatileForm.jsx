7352698b1cb7a4ddc08f36ba5758ba5e42ae380a
tari45800 <tari45800@gmail.com>
2023-04-18 00:14:54 +0900

    feat:VersatileForm 컴포넌트 추가

---
+++ client/src/components/questionPageComponents/VersatileForm.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+
+const VersatileFormContainer = styled.div`
+
+`
+
+
+function VersatileForm() {
+
+
+  return (
+    <VersatileFormContainer>
+      <div>태스트입니다</div>
+    </VersatileFormContainer>
+  );
+}
+
+export default VersatileForm;
