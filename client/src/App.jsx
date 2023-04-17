b09c1b0be7e3b012b9f60683669a07c19056b3ff
SEB_FE_43_유호균 <74657430+ghrbsaod@users.noreply.github.com>
2023-04-17 13:39:36 +0900

    Footer 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import { GlobalStyles } from "./styles/GlobalStyles";
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <div className="App">test</div>
+      
+      
+
+      <Footer />
     </>
   );
 }
