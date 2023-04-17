7b15aa6ae9b01ac301f49177513841e00ed1c714
crowwan <crowwan@naver.com>
2023-04-17 12:26:51 +0900

    feat: a태그 기본 스타일 추가

--- client/src/styles/GlobalStyles.jsx
+++ client/src/styles/GlobalStyles.jsx
@@ -57,4 +57,11 @@ export const GlobalStyles = createGlobalStyle`
     border-collapse: collapse;
     border-spacing: 0;
   }
+  a{
+    color: inherit;
+    text-decoration: none;
+  }
+  a:visited{
+    color: inherit;
+  }
 `;
