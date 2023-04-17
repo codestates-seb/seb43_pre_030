726b535acf2ae97afaef0ae844b913ce3ce3f285
crowwan <crowwan@naver.com>
2023-04-17 13:38:35 +0900

    feat: 해더 컴포넌트에 필요한 컴포넌트들 제작 완료

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
+import SearchHint from "./SearchHint";
+
+const StyledInputContainer = styled.div`
+  position: relative;
+
+  color: var(--font-color-bold);
+  > div:first-child {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 15%;
+    left: 0.5rem;
+    font-size: 1.5rem;
+  }
+`;
+const StyledForm = styled.form`
+  flex: 1 0 0;
+`;
+const StyledInput = styled.input`
+  border: 1.5px solid #babfc4;
+  outline: none;
+  width: 100%;
+  border-radius: 3px;
+  padding: 0.5rem 0.5rem 0.5rem 2.5rem;
+`;
+function SearchBar() {
+  const [showHint, setShowHint] = useState(false);
+  const onSubmitHandler = e => {
+    e.preventDefault();
+  };
+  const invertShowState = () => {
+    setShowHint(prev => !prev);
+  };
+
+  return (
+    <StyledForm onSubmit={onSubmitHandler}>
+      <StyledInputContainer>
+        <div>
+          <AiOutlineSearch color="#838c95" />
+        </div>
+        <StyledInput placeholder="Search..." onFocus={invertShowState} onBlur={invertShowState} />
+        {showHint && <SearchHint />}
+      </StyledInputContainer>
+    </StyledForm>
+  );
+}
+
+export default SearchBar;
