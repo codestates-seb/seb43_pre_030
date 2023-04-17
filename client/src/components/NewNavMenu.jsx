0de55e24750bd7ec70346b021183711fe5b0c041
tari45800 <tari45800@gmail.com>
2023-04-17 17:02:36 +0900

    feat:연구중인 nav

--- client/src/components/NewNavMenu.jsx
+++ client/src/components/NewNavMenu.jsx
@@ -31,7 +31,7 @@ const MenuContainer = styled.li`
     font-size: 0.6rem;
     color: black;
     font-weight: bold;
-    background-color: #fff;
+    background-color: #f8f9f9;
   }
 
 
@@ -39,10 +39,24 @@ const MenuContainer = styled.li`
     display: block;
     position: absolute;
     top: 0;
-    left: 0;
-    height: ${props => props.menusLength} * 34px;
+    left: 5px;
+    /* height: ${props => props.menusLength} * 34px; */
+    height: 340px;
     width: 0.5px;
     background-color: #aaa;
+    z-index: -1;
+  }
+
+  & .navBar{
+    display: block;
+    position: absolute;
+    top: ${props => props.idx} *34px;
+    left: 4px;
+    height: 34px;
+    width: 3px;
+    background-color: #F48225; 
+    border-radius:5px;
+    z-index: 1;
   }
 
 `
@@ -53,14 +67,16 @@ function NewNavMenu({menusLength, menuFoucs, setFocus, menuIdx, el}) {
   const setActive = (e) => {
     setFocus(e.target.value)
     console.log(menusLength)
+    console.log(menuIdx)
   }
 
   return(
     // 클릭 시 menuFoucs에 따라서 액티브 클래스 적용
-    <MenuContainer menusLength={menusLength} value={menuIdx} className={`${menuIdx === menuFoucs && el.isContent ? 'active' : ''} ${el.isContent ? 'content' : 'title'}`}
+    <MenuContainer menusLength={menusLength} idx={menuIdx} value={menuIdx} className={`${menuIdx === menuFoucs && el.isContent ? 'active' : ''} ${el.isContent ? 'content' : 'title'}`}
     onClick={(e) => {setActive(e)}}>
       <span>{el.emoji}{el.title}
         <span className={`${el.contentsNav ? 'navNav' : ''}`}> </span>
+        <span className={`${el.contentsNav ? 'navBar' : ''}`}> </span>
       </span>
     </MenuContainer>
   )
