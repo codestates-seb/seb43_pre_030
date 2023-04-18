6d7ad8cbde7a7deb26e194459a178ba4b92e9205
tari45800 <tari45800@gmail.com>
2023-04-18 13:42:06 +0900

    feat: 박스 스타일 추가

--- client/src/components/questionPageComponents/WritingGuideBox.jsx
+++ client/src/components/questionPageComponents/WritingGuideBox.jsx
@@ -3,16 +3,29 @@ import styled from "styled-components";
 
 
 const WritingGuideBoxContainer = styled.div`
+  width: 70%;
+  border: 1px solid #A5CFED;
+  border-radius: 0.2rem;
+  background-color:#EBF4FA;
+  padding: 1.5rem;
+  margin-top: 1rem;
 
+  h1{
+    font-size: 1.5rem;
+    font-weight: normal;
+    margin-bottom: 0.5rem;
+  }
 `
 
-
-function WritingGuideBox() {
+function WritingGuideBox({title, content}) {
 
 
   return (
     <WritingGuideBoxContainer>
-      <div>태스트입니다</div>
+      <h1>{title}</h1>
+      {content.split(`\n`).map((el, index) => (
+        <div key={index}>{el}</div>
+      ))}
     </WritingGuideBoxContainer>
   );
 }
