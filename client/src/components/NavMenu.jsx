0ab64a35aa603c256b26fdabe081bb99f21122e8
crowwan <crowwan@naver.com>
2023-04-18 13:16:24 +0900

    feat: 라우터 적용 브랜치 푸쉬로 인한 커밋

--- client/src/components/NavMenu.jsx
+++ client/src/components/NavMenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const MenuContainer = styled.li`
@@ -10,56 +11,61 @@ const MenuContainer = styled.li`
   cursor: pointer;
 
   //span기본 css
-  & span{
+  & span {
     margin-left: 10px;
     letter-spacing: 0.5px;
     //클릭시 이벤트 제어
     pointer-events: none;
   }
-  
+
   //클릭 시 변화 css
   &.active {
-    color:  var(--font-color-bold);
+    color: var(--font-color-bold);
     font-weight: bold;
     background-color: #66666615;
-    box-shadow: -3px 0 0 0 var(--primary-color) inset; 
+    box-shadow: -3px 0 0 0 var(--primary-color) inset;
     transition: 0.1s;
   }
 
   //클릭 시 변화가 있는 박스
-  &.content{
-    & span{
-        margin-left: 30px;
+  &.content {
+    & span {
+      margin-left: 30px;
     }
   }
 
   //클릭시 변화가 없는 박스
-  &.title{
+  &.title {
     padding-top: 16px;
     font-size: 0.6rem;
     font-weight: bold;
     color: var(--font-color-bold);
   }
 
-  &.content:hover{
+  &.content:hover {
     color: var(--font-color-bold);
   }
-`
+`;
 
-function NavMenu({menuFoucs, setFocus, menuIdx, el}) {
-
+function NavMenu({ menuFoucs, setFocus, menuIdx, el }) {
+  const navigation = useNavigate();
   // 클릭된 매뉴의 값을 menuFoucs에 적용하는 함수
-  const setActive = (e) => {
-    setFocus(e.target.value)
-  }
+  const setActive = e => {
+    setFocus(e.target.value);
+  };
 
-  return(
+  return (
     // 클릭 시 menuFoucs에 따라서 액티브 클래스 적용
-    <MenuContainer value={menuIdx} className={`${menuIdx === menuFoucs && el.isContent ? 'active' : ''} ${el.isContent ? 'content' : 'title'}`}
-    onClick={(e) => {setActive(e)}}>
+    <MenuContainer
+      value={menuIdx}
+      className={`${menuIdx === menuFoucs && el.isContent ? "active" : ""} ${el.isContent ? "content" : "title"}`}
+      onClick={e => {
+        setActive(e);
+      }}
+    >
       <span>{el.title}</span>
     </MenuContainer>
-  )
+  );
 }
 
 export default NavMenu;
