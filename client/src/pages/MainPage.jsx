10ba1bd8ba28b8db22b0051a90f50902ac4ba1c4
crowwan <crowwan@naver.com>
2023-04-18 13:16:00 +0900

    feat: 공통 레이아웃 컴포넌트 분리 / 메인 페이지 개발 시작

---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Template from "./Template";
+import Main from "../layouts/Main";
+import MainHeaderSection from "../components/MainHeaderSection";
+
+function MainPage() {
+  return (
+    <Main>
+      <MainHeaderSection />
+    </Main>
+  );
+}
+
+export default MainPage;
