b9002ea1d82a1b127fc1a7dee0699d36af363fc8
crowwan <crowwan@naver.com>
2023-04-18 14:12:17 +0900

    feat: 메인 화면 상단 컴포넌트 디자인 완료

--- client/src/components/MainHeaderSection.jsx
+++ client/src/components/MainHeaderSection.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import { StyledTitle } from "../styles/StyledTItle";
 import { ButtonMain } from "./ButtonMain";
+import Button from "./Button";
 
 const StyledContainer = styled.div`
   display: flex;
-  background-color: gray;
+  flex-direction: column;
+  gap: 2rem;
   padding: 1.5rem;
 `;
 const StyledTopSection = styled.div`
@@ -13,7 +15,19 @@ const StyledTopSection = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
-const StyledBottomSection = styled.div``;
+const StyledBottomSection = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  gap: 0.1rem;
+`;
+
+const MainButton = Button({
+  bg: "#fff",
+  fontColor: "var(--font-color-light)",
+  border: "1px solid #BABFC4",
+  hoverBg: "#E3E5E8",
+});
 function MainHeaderSection() {
   return (
     <StyledContainer>
@@ -21,6 +35,11 @@ function MainHeaderSection() {
         <StyledTitle>All Questions</StyledTitle>
         <ButtonMain>Ask Question</ButtonMain>
       </StyledTopSection>
+      <StyledBottomSection>
+        <MainButton>hottest</MainButton>
+        <MainButton>newest</MainButton>
+        <MainButton>oldest</MainButton>
+      </StyledBottomSection>
     </StyledContainer>
   );
 }
