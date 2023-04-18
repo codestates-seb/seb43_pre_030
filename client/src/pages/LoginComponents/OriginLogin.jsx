cf557ee6bcebda8872bba425257c52b27af801f6
Suam Kang <ksa199653@gmail.com>
2023-04-18 16:29:14 +0900

    feat : 소셜 버튼 색상 추가

--- client/src/pages/LoginComponents/OriginLogin.jsx
+++ client/src/pages/LoginComponents/OriginLogin.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Button from "../../components/Button";
+
+const StyledOriginLoginWrapper = styled.div`
+  width: 18rem;
+  padding: 1rem;
+  border: 1px solid black;
+  background-color: #fff;
+  border-radius: 0.3rem;
+  box-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.05), 0 1rem 2rem rgba(0, 0, 0, 0.05);
+`;
 
 function OriginLogin() {
-  return <div>test</div>;
+  const [email, setEamil] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <StyledOriginLoginWrapper>
+      <div className="email" />
+      <div className="pw" />
+      <div className="button">
+        <Button>Log in</Button>
+      </div>
+    </StyledOriginLoginWrapper>
+  );
 }
 
 export default OriginLogin;
--- client/src/styles/GlobalStyles.jsx
+++ client/src/styles/GlobalStyles.jsx
@@ -4,6 +4,8 @@ export const GlobalStyles = createGlobalStyle`
   :root {
     --primary-color: #f48225; // 포인트 색상
     --btn-bg-color: #0a95ff; // 기본 버튼 배경 색상
+    --btn-github-bg-color: #2f3337 // 깃헙버튼 색상
+    --btn-facebook-bg-color: #385499 // 페북버튼 색상
     --al-color: #0074cc; // 기본 a 링크 색상
     --font-color-light: #525960; // 연한 폰트 기본 색상
     --font-color-bold:#0c0d0e; // 진한 폰트 기본 색상
@@ -11,7 +13,7 @@ export const GlobalStyles = createGlobalStyle`
     --tag-font-color: #39739d; // 태그 폰트 색상
     --aside-bg-bold: #fcf2d4; // 진한 사이드 배경 색상
     --aside-bg-light: #fdf6e2; // 연한 사이드 배경 색상
-    --border-default-color: #d6d9dc;
+    --border-default-color: #d6d9dc; // 보더 기본 색상
   }
   * {
     box-sizing: border-box;
