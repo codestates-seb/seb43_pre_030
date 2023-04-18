b224dfe318810b312866566bef21b0df01a0baf8
tari45800 <tari45800@gmail.com>
2023-04-18 13:45:42 +0900

    feat: 박스 스타일 추가

--- client/src/components/questionPageComponents/TipBox.jsx
+++ client/src/components/questionPageComponents/TipBox.jsx
@@ -3,16 +3,42 @@ import styled from "styled-components";
 
 
 const TipBoxContainer = styled.div`
+  width: 30%;
+  border: 1px solid var(--question-page-form-border-color);
+  border-radius: 0.2rem;
+  background-color:var(--question-page-bg-color);
+  margin-top: 1rem;
+  border: 1px solid #D6D9DC;
+  box-shadow: 0 2px 5px 1px #D6D9DC;
 
+  h1{
+    font-size: 1rem;
+    font-weight: normal;
+    padding: 1rem;
+  }
+
+  & .divContainer{
+    background-color: #fff;
+    padding: 1rem;
+    font-size: 0.8rem;
+    border-top: 1px solid #D6D9DC;
+    border-bottom-right-radius: 0.2rem;
+    border-bottom-left-radius: 0.2rem;
+  }
 `
 
 
-function TipBox() {
+function TipBox({title, content}) {
 
 
   return (
     <TipBoxContainer>
-      <div>태스트입니다</div>
+      <h1>{title}</h1>
+        <div className="divContainer">
+      {content.split(`\n`).map((el, idx) => (
+        <div key={idx}>{el}</div>
+      ))}
+    </div>
     </TipBoxContainer>
   );
 }
