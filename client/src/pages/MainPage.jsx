0748c02e852628e0d563fc016b167af0d825e6c4
crowwan <crowwan@naver.com>
2023-04-18 14:13:09 +0900

    feat: 메인 패이지 컴포넌트 생성

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Template from "./Template";
 import Main from "../layouts/Main";
 import MainHeaderSection from "../components/MainHeaderSection";
+import MainItem from "../components/MainItem";
 
 function MainPage() {
   return (
     <Main>
       <MainHeaderSection />
+      {[1, 2, 4, 5].map(_ => (
+        <MainItem />
+      ))}
     </Main>
   );
 }
