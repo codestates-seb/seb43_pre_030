a5068335670b30b99ea578c4841d241f56d46f15
crowwan <crowwan@naver.com>
2023-04-18 09:50:11 +0900

    feat: 메뉴 드롭다운 컨텐츠 추가

--- client/src/components/HeaderMenu.jsx
+++ client/src/components/HeaderMenu.jsx
@@ -1,9 +1,11 @@
 import { AiOutlineUser } from "react-icons/ai";
-import { BsMenuUp } from "react-icons/bs";
+import { BsMenuUp, BsSun, BsSunFill } from "react-icons/bs";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { StyledDropDown } from "../styles/StyledDropDown";
 import { useDropDown } from "../hooks/useDropDown";
+import { ButtonMain } from "./ButtonMain";
+import Button from "./Button";
 
 const StyledMenuContainer = styled.div`
   display: flex;
@@ -14,26 +16,29 @@ const StyledMenuContainer = styled.div`
   }
 `;
 
-const StyledUserDropDown = styled(StyledDropDown)`
+const StyledMenuDropDown = styled(StyledDropDown)`
   top: 100%;
-  right: -1rem;
+  right: -0.5rem;
+  min-width: 180px;
+  color: var(--al-color);
+  cursor: auto;
   &::after {
     position: absolute;
-    right: 10%;
-  }
-`;
-const StyledMenuDropDown = styled(StyledUserDropDown)`
-  right: -0.5rem;
-  &::after {
     right: 5%;
   }
+`;
+const StyledFlexBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 const StyledWrapper = styled.div`
   position: relative;
 `;
 
 function HeaderMenu() {
-  const [menuDropDown, closeMenu, onMenuClick] = useDropDown(false);
+  const [menuDropDown, _, onMenuClick] = useDropDown(false);
   return (
     <StyledMenuContainer>
       <Link to="/profile">
@@ -41,7 +46,14 @@ function HeaderMenu() {
       </Link>
       <StyledWrapper>
         <BsMenuUp size="2rem" color="var(--font-color-light)" onClick={onMenuClick} />
-        {menuDropDown && <StyledMenuDropDown>text</StyledMenuDropDown>}
+        {menuDropDown && (
+          <StyledMenuDropDown onClick={e => e.stopPropagation()}>
+            <StyledFlexBox>
+              <ButtonMain>log out</ButtonMain>
+              <BsSunFill size="1.5rem" cursor="pointer" />
+            </StyledFlexBox>
+          </StyledMenuDropDown>
+        )}
       </StyledWrapper>
     </StyledMenuContainer>
   );
