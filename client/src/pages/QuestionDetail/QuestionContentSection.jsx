fa8fc482a7a40b8f79cf51dc69c459d311075a50
Suam Kang <ksa199653@gmail.com>
2023-04-20 14:58:26 +0900

    feat : 컨텐츠 내용 수정

--- client/src/pages/QuestionDetail/QuestionContentSection.jsx
+++ client/src/pages/QuestionDetail/QuestionContentSection.jsx
@@ -1,27 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 
+// 잘문 컨텐츠 전체 내용 래퍼
 const StyledContentWrapper = styled.div`
   display: flex;
   align-items: flex-start;
 `;
 
-function QuestionContentSection({ updateAt }) {
+// 컨텐츠 안 util기능들 래퍼
+const StyledUtilsWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 1rem 0;
+  padding-top: 0.25rem;
+
+  .modified-date {
+    padding-top: 0.6rem;
+    font-size: 0.7rem;
+    color: var(--font-color-light);
+  }
+`;
+
+const UtilsOptions = styled.div`
+  display: flex;
+  color: var(--font-color-light);
+  font-size: 0.7rem;
+  margin-left: 0.5rem;
+
+  button {
+    height: 1rem;
+    margin-top: 0.125rem;
+    padding-top: 0.4rem;
+    color: var(--font-color-light);
+    font-size: 0.7rem;
+    background-color: transparent; // 투명하게
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+// 컨텐츠 내용
+function QuestionContentSection({ id, userId, body, createAt, modifiedAt }) {
   return (
     <StyledContentWrapper>
       <div className="vote">vote</div>
       <div className="content-container">
         <div className="real-content">내용</div>
         <div className="tag-wrapper">태그 래퍼</div>
-        <div className="util-wrapper">
-          <div className="features-option">
+        <StyledUtilsWrapper>
+          <UtilsOptions>
             <button type="button">Share</button>
             <button type="button">Edit</button>
             <button type="button">Delete</button>
-          </div>
-          {updateAt && <span>edited {updateAt}</span>}
+          </UtilsOptions>
+          {modifiedAt && <span className="modified-date">edited {modifiedAt}</span>}
           <div className="user-info">유저정보</div>
-        </div>
+        </StyledUtilsWrapper>
       </div>
     </StyledContentWrapper>
   );
--- client/src/pages/QuestionDetail/QuestionDetailPage.jsx
+++ client/src/pages/QuestionDetail/QuestionDetailPage.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 import Main from "../../layouts/Main/Main";
 import QuestionHeaderSection from "./QuestionHeaderSection";
 import QuestionContentSection from "./QuestionContentSection";
@@ -27,38 +29,67 @@ function AnswersHeader({ count }) {
 
 // 상세페이지
 function QuestionDetailPage() {
+  const { id } = useParams();
+  // 질문글 id에 따른 데이터 요청 주소
+  const url = `http://localhost:3001/question/${id}`;
+  // 답변글 id에 따른 데이터 요청 주소
+  const url2 = `http://localhost:3001/answer/${id}`;
+
   // 서버에서 받아온 질문 데이터 상태
-  const [questionData, setQuestionData] = useState(null);
+  const [questionData, setQuestionData] = useState({});
   const [isPending, setIsPending] = useState(false);
 
-  if (isPending && questionData === null) {
-    return <Loading />; // 로딩 이미지 추가
-  }
-  if (questionData) {
-    return (
-      <Main>
-        <QuestionHeaderSection
-          title={questionData.title}
-          createAt={questionData.createdAt}
-          updateAt={questionData.updateAt}
-          views={questionData.views}
-        />
-        {/* 내용 */}
-        <QuestionContentSection />
-        {/* 답변들 */}
-        {questionData.answers.length > 0 && (
-          <>
-            <AnswersHeader count={questionData.answers.length} />
-            {questionData.answers.map((answer, idx) => (
-              <QuestionContentSection key={idx} />
-            ))}
-          </>
-        )}
-        {/* 답변 작성폼 */}
-        <AnswerCreateSection />
-      </Main>
-    );
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+      try {
+        const { data } = await axios(url);
+        console.log(data);
+        setQuestionData({ ...data });
+      } catch (err) {
+        console.error(err);
+      }
+      setIsPending(false);
+    };
+    fetchData();
+  }, [url]);
+
+  console.log(questionData);
+
+  return (
+    <>
+      {isPending && <Loading />}
+      {questionData && (
+        <Main>
+          <QuestionHeaderSection
+            title={questionData.title}
+            createAt={questionData.created_at}
+            updateAt={questionData.modified_at}
+            tags={questionData.tag}
+          />
+          {/* 내용 */}
+          <QuestionContentSection
+            id={questionData.id}
+            userId={questionData.user_id}
+            body={questionData.body}
+            createdAt={questionData.created_at}
+            updatedAt={questionData.modified_at}
+          />
+          {/* 답변들 */}
+          {questionData.answer.length > 0 && (
+            <>
+              <AnswersHeader count={questionData.answer} />
+              {questionData.answers.map((answer, idx) => (
+                <QuestionContentSection key={idx} />
+              ))}
+            </>
+          )}
+          {/* 답변 작성폼 */}
+          <AnswerCreateSection />
+        </Main>
+      )}
+    </>
+  );
 }
 
 export default QuestionDetailPage;
