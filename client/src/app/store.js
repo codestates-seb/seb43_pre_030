c373a6eb1ea2db7f140b25903833d78e5e117dc1
Suam Kang <ksa199653@gmail.com>
2023-04-24 14:09:53 +0900

    feat: 로그인 slice 구성

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import dataReducer from "../features/dataSlice";
+import loginReducer from "../features/loginSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     data: dataReducer,
+    login: loginReducer,
   },
 });
 export default store;
