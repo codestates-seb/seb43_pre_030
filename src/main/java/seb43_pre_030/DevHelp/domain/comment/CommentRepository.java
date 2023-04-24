83a12536dd06bd521599cb9dab66c90f1c53bdac
mins <m2insapp@gmail.com>
2023-04-24 14:08:43 +0900

    Remove all files from main branch

--- src/main/java/seb43_pre_030/DevHelp/domain/comment/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package seb43_pre_030.DevHelp.commet.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/seb43_pre_030/DevHelp/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package seb43_pre_030.DevHelp.exception;
-
-public class UserAlreadyExistsException extends Exception {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-
-}
