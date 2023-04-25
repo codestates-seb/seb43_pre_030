baa06fa8101cbb4f3e2f5d0951b52e5a68714936
hunm719 <hunm719@naver.com>
2023-04-26 01:02:36 +0900

    04.26 01:00

--- src/main/java/seb43_pre_030/DevHelp/DevHelpApplication.java
+++ src/main/java/seb43_pre_030/DevHelp/DevHelpApplication.java
@@ -3,7 +3,9 @@ package seb43_pre_030.DevHelp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan({"seb43_pre_030.DevHelp.domain.tag.mapper"})
 public class DevHelpApplication {
